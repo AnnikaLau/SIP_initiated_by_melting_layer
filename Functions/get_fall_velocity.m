fc7588dceb0405c6b1ad040546d54850de1dda6b
AnnikaLau <annika.lauber@env.ethz.ch>
2021-01-27 09:08:51 +0100

    Function does not have to take cD object as input anymore

--- Functions/get_fall_velocity.m
+++ Functions/get_fall_velocity.m
@@ -1,16 +1,10 @@
-function v = get_fall_velocity(temp1)
-v = zeros(length(temp1.class),1);
+function v = get_fall_velocity(d,class)
+v = zeros(length(d),1);
 
-%Find the equivalent particle area diameter
-i = find(contains(temp1.metricnames,'pDiam')==1);
-d = temp1.metricmat(:,i);
-%Find the major axis size
-i = find(contains(temp1.metricnames,'majsizRescale')==1);
-L = temp1.metricmat(:,i);
 %Droplets smaller than 60mum
-ws = contains(temp1.cpType,'Water') & d<60e-6;
+ws = contains(class,'Water') & d<60e-6;
 %Droplets larger than 60mum
-wl = contains(temp1.cpType,'Water') & d>=60e-6;
+wl = contains(class,'Water') & d>=60e-6;
 
 %Calculate the fall velocities of cloud droplets
 %Rogers and Yan 1989 (book p. 203)
@@ -20,10 +14,10 @@ v(ws) = (k1*(d(ws).^2))/4;
 v(wl) = k2*d(wl)./2;
 
 %Ice crystals considered as plates
-p = contains(temp1.cpType,'Ice_Plate') | contains(temp1.cpType,'Ice_Unidentified');
-v(p) = 156*L(p).^0.86;
+p = contains(class,'Ice_Plate') | contains(class,'Ice_Unidentified');
+v(p) = 156*d(p).^0.86;
 %Ice crystals considered as lump graupel
-g = contains(temp1.cpType,'Ice_Aged') | contains(temp1.cpType,'Ice_Column') | contains(temp1.cpType,'Ice_Irregular');
-v(g) = 124*L(g).^0.66;
+g = contains(class,'Ice_Aged') | contains(class,'Ice_Column') | contains(class,'Ice_Irregular');
+v(g) = 124*d(g).^0.66;
 end
 
